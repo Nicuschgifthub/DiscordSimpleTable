e32a85c9fae9f5145b0e9d935289691896e2971f
Nicusch <62447448+Nixkuchengifthub@users.noreply.github.com>
2024-07-28 20:36:48 +0200

    Added two functions & updated Readme + added docs inside of the class

--- DiscordSimpleTable.js
+++ DiscordSimpleTable.js
@@ -1,28 +1,62 @@
 class DiscordSimpleTable {
-    constructor(columns) {
+    /**
+     * Creates a new DiscordSimpleTable instance.
+     * @param {Array} columns - An array of column objects, each with `label` and `key` properties.
+     * @param {string} [spaceString=' '] - A string used for spacing in the table.
+     */
+    constructor(columns, spaceString = ' ') {
         this._columns = columns;
-        this._stringOffset = 0;
         this._items = [];
+
+        this._stringOffset = 0;
         this._widthForKeys = {};
+
         this._verticalBar = false;
+        this._spaceString = spaceString;
+        this._indexLabel = '#';
     }
 
+    /**
+     * Sets the string offset for spacing.
+     * @param {number} [offset=2] - The number of spaces to offset.
+     * @returns {DiscordSimpleTable} The current instance for chaining.
+     */
     setStringOffset(offset = 2) {
         this._stringOffset = offset;
         return this;
     }
 
+    /**
+     * Sets the label for the index column.
+     * @param {string} label - The label for the index column.
+     * @returns {DiscordSimpleTable} The current instance for chaining.
+     */
+    setIndexLabel(label) {
+        this._indexLabel = label;
+        return this;
+    }
+
+    /**
+     * Adds an index column to the table.
+     * @param {number} [startValue=1] - The starting value for the index.
+     * @returns {DiscordSimpleTable} The current instance for chaining.
+     */
     addIndex(startValue = 1) {
         this._items = this._items.map((item, index) => {
             return { ...item, index: startValue + index };
         });
 
-        this._columns.unshift({ label: '#', key: 'index' });
+        this._columns.unshift({ label: this._indexLabel, key: 'index' });
         this._widthForKeys['index'] = 0;
 
         return this;
     }
 
+    /**
+     * Sets the input data for the table from a JSON array.
+     * @param {Array} jsonArray - The JSON array of data objects.
+     * @returns {DiscordSimpleTable} The current instance for chaining.
+     */
     setJsonArrayInputs(jsonArray) {
         jsonArray.forEach((row) => this._items.push(row));
 
@@ -37,11 +71,20 @@ class DiscordSimpleTable {
         return this;
     }
 
+    /**
+     * Adds or removes a vertical bar between rows.
+     * @param {boolean} [add=true] - Whether to add a vertical bar.
+     * @returns {DiscordSimpleTable} The current instance for chaining.
+     */
     addVerticalBar(add = true) {
         this._verticalBar = add;
         return this;
     }
 
+    /**
+     * Updates the width for each key based on the data and column labels.
+     * @private
+     */
     _updateWidthForKeys() {
         for (let key in this._widthForKeys) {
             const maxLength = Math.max(
@@ -52,16 +95,33 @@ class DiscordSimpleTable {
         }
     }
 
+    /**
+     * Adds extra spaces to a string to meet the desired length.
+     * @param {string} string - The input string.
+     * @param {number} count - The desired length.
+     * @returns {string} The input string padded with extra spaces.
+     */
     extraSpaces(string, count) {
         const spacesToAdd = Math.max(0, count - string.length);
-        return ' '.repeat(spacesToAdd);
+        return this._spaceString.repeat(spacesToAdd);
     }
 
+    /**
+     * Appends extra spaces to a string to meet the desired length.
+     * @param {string} string - The input string.
+     * @param {number} count - The desired length.
+     * @returns {string} The input string appended with extra spaces.
+     */
     stringExtraSpaces(string, count) {
         const spacesToAdd = Math.max(0, count - string.length);
-        return string + ' '.repeat(spacesToAdd);
+        return string + this._spaceString.repeat(spacesToAdd);
     }
 
+    /**
+     * Creates the top header row of the table.
+     * @private
+     * @returns {string} The formatted top header row.
+     */
     _makeTopHeader() {
         let header = "`";
 
@@ -82,6 +142,11 @@ class DiscordSimpleTable {
         return header;
     }
 
+    /**
+     * Creates the entries rows of the table.
+     * @private
+     * @returns {string} The formatted entries rows.
+     */
     _makeEntries() {
         let string = "";
 
@@ -100,6 +165,10 @@ class DiscordSimpleTable {
         return string;
     }
 
+    /**
+     * Builds the complete table as a string.
+     * @returns {string} The formatted table as a string.
+     */
     build() {
         this._updateWidthForKeys();
 
@@ -111,4 +180,4 @@ class DiscordSimpleTable {
 }
 
 
-module.exports = DiscordSimpleTable ;
+module.exports = DiscordSimpleTable;
--- README.md
+++ README.md
@@ -1,44 +1,50 @@
 # DiscordSimpleTable by Nicusch
 
-Can be used for Discord to make tables
+DiscordSimpleTable provides a way to create and format tables for display in Discord messages.
+It allows customization of column headers, row data, and table appearance,
+including options for adding index columns and vertical bars.
 
 ```js
-  // Other imports...
-  const DiscordSimpleTable = require('discord-simpletable');
+// Other imports...
+const DiscordSimpleTable = require('discord-simpletable');
 
-  const data = [
-    { name: "Nicusch", sizetype: "Big", count: 20 },
-    { name: "Noner", sizetype: "Medium", count: 13 },
-    { name: "Don", sizetype: "Extralarge", count: 55 },
-    { name: "Han", sizetype: "ExtraMediumLarge", count: 33 },
-  ]
+const data = [
+  { name: "Nicusch", sizetype: "Big", count: 20 },
+  { name: "Noner", sizetype: "Medium", count: 13 },
+  { name: "Don", sizetype: "Extralarge", count: 55 },
+  { name: "Han", sizetype: "ExtraMediumLarge", count: 33 },
+];
 
-  const columns = [
-    { label: 'Username', key: 'name' },
-    { label: 'Bucket Size', key: 'sizetype' },
-    { label: 'Count', key: 'count' }
-  ];
+const columns = [
+  { label: 'Username', key: 'name' },
+  { label: 'Bucket Size', key: 'sizetype' },
+  { label: 'Count', key: 'count' },
+];
 
-  const tableString = new DiscordSimpleTable(columns)
-    .setJsonArrayInputs(data) // defines the data
-    .setStringOffset(2) // adds an offset to the words like 2 means "Username  Bucket Size"
-    .addVerticalBar() // adds an vertical Bar
-    .addIndex(1) // adds a new column called index and represented as "#" the argumnet "1" defines the start count like 1,2,3,...
-    .build(); // builds the table (async)
+const tableString = new DiscordSimpleTable(columns)
+  .setJsonArrayInputs(data)  // Defines the data
+  .setStringOffset(2)  // Adds an offset to the words (class default is 0)
+  .addVerticalBar()  // Adds a vertical bar
+  .setIndexLabel('#')  // Sets the index column label (class default is "#")
+  .addIndex(1)  // Adds an index column starting at 1
+  .build();  // Builds the table
 
-  // Note .addIndex(1) needs to be called after .setJsonArrayInputs(data)
-  // Reason is that .addIndex(param) is the function which adds another data entry. Otherwise .setJsonArrayInputs(data) would replace the entire data
-  // This will be changed later to be called without needing structure
+// Note: .addIndex(1) should be called after .setJsonArrayInputs(data)
+// because .addIndex(param) adds another data entry. Otherwise, .setJsonArrayInputs(data) would replace the entire data.
 
-  new Embed()
-    .setColor(config.embeds.colors.info)
-    .setDescription(tableString)
-    .interactionResponse(interaction);
+// "const tableString" is now ready to be used
+// as an example in Discord.js Api
+
+new Embed()
+  .setColor(config.embeds.colors.info)
+  .setDescription(tableString)
+  .interactionResponse(interaction);
+
+// Note Embed is my own custom class
 ```
+As an Discord Embed it looks like this:
 
-This could looks like this:
-
-![Discord Table Example1](https://raw.githubusercontent.com/Nicuschgifthub/DiscordSimpleTable/master/images/example.png)
+![Discord Table Example1](https://raw.githubusercontent.com/Nicuschgifthub/DiscordSimpleTable/master/images/example1.png)
 
 
 You can also leave out the ".addVerticalBar()" and ".addIndex(1)"
@@ -53,7 +59,21 @@ You can also leave out the ".addVerticalBar()" and ".addIndex(1)"
 //...
 ```
 
-![Discord Table Example1](https://raw.githubusercontent.com/Nicuschgifthub/DiscordSimpleTable/master/images/example2.png)
+![Discord Table Example2](https://raw.githubusercontent.com/Nicuschgifthub/DiscordSimpleTable/master/images/example2.png)
 
-The best thing is that the rows and columns will have good spaceing no matter which word on name is longer etc.
-Quick Note for Discord.js really long rows are not the best option as they differ from devices.
+The rows and columns will have proper spacing, regardless of word length.
+For Discord.js, very long rows may not display well on all devices due to differences in screen sizes.
+
+
+Example to change empty space to '-' 
+
+```js
+//...
+  const tableString = new DiscordSimpleTable(columns, '-')
+//...
+```
+
+## Install
+```bash
+npm install discord-simpletable
+```
